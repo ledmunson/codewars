2dcc324952e5704b29566f328502066a05164749
ledmunson <ledmunson90@gmail.com>
2020-08-08 11:51:48 -0700

    Solution with best practice.

--- exesAndOhs.js
+++ exesAndOhs.js
@@ -10,7 +10,39 @@ XO("zzoo") => false */
 
 // Javascript
 
+// My initial solution.
 function XO(str) {
-    temp = str.split();
-    console.log(temp);
+    // Initialize variables to count X's and O's. 
+    var exes = 0;
+    var ohs = 0;
+
+    // Clean up and format the input string.
+    str = str.toLowerCase();
+    str = str.trim();
+    str = str.split("");
+
+    // Count the number of X's and O's.
+    for (i=0;i<str.length;i++) {
+      if (str[i] == "x") {
+        exes++;
+      }
+      else if (str[i] == "o") {
+        ohs++;
+      }
+    }
+
+    // Return true if number of X's equals number of O's. Return false if not.
+    if (exes == ohs) {
+      return true;
+    }
+    else {
+      return false;
+    }
+}
+
+// Best practice.
+function XO(str) {
+    let x = str.match(/x/gi);
+    let o = str.match(/o/gi);
+    return (x && x.length) === (o && o.length);
 }
